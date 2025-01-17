4b61f7ad54754d6bd37e99d670f3ff52b941aac3
Álvaro Vega Gómez <alvarvg@gmail.com>
2025-01-17 13:49:19 +0100

    changed the route for better use

--- srcs/backend/api/urls.py
+++ srcs/backend/api/urls.py
@@ -6,15 +6,15 @@ from . import views
 
 urlpatterns = [
     path("", views.default, name="default"),
-    path("add-user/", views.add_user, name="add-user"),
-    path("delete-user/", views.delete_user, name="delete-user"),
-    path("add-chat/", views.add_chat, name="add-chat"),
-    path("delete-chat/", views.delete_chat, name="delete-chat"),
-    path("add-message/", views.add_message, name="add-message"),
-    path("delete-message/", views.delete_message, name="delete-message"),
+    path("add/user/", views.add_user, name="add-user"),
+    path("delete/user/", views.delete_user, name="delete-user"),
+    path("add/chat/", views.add_chat, name="add-chat"),
+    path("delete/chat/", views.delete_chat, name="delete-chat"),
+    path("add/message/", views.add_message, name="add-message"),
+    path("delete/message/", views.delete_message, name="delete-message"),
     # TODO:
-    # path("add-match/", views.add_match, name="add-match"),
-    # path("delete-match/", views.delete_match, name="delete-match"),
-    # path("add-tournament/", views.add_tournament, name="add-tournament"),
-    # path("delete-tournament/", views.delete_tournament, name="delete-tournament"),
+    # path("add/match/", views.add_match, name="add-match"),
+    # path("delete/match/", views.delete_match, name="delete-match"),
+    # path("add/tournament/", views.add_tournament, name="add-tournament"),
+    # path("delete/tournament/", views.delete_tournament, name="delete-tournament"),
 ]
