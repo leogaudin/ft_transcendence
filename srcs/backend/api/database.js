18aed7a6eead9e564599d15fff03d44aa35533a8
Álvaro Vega Gómez <alvarvg@gmail.com>
2025-02-05 12:19:43 +0100

    added database fields

--- srcs/backend/api/database.js
+++ srcs/backend/api/database.js
@@ -12,15 +12,35 @@ db.serialize(() => {
     `CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT NOT NULL UNIQUE,
+    alias TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    is_online BOOLEAN DEFAULT 0,
+    last_login DATETIME DEFAULT CURRENT_TIMESTAMP,
+    wins INTEGER DEFAULT 0 CHECK (wins >= 0),
+    losses INTEGER DEFAULT 0 CHECK (losses >= 0)
   )`,
     (err) => {
       if (err) {
         return console.error("Error creating table:", err.message);
       }
       console.log("User table ready.");
+    },
+  );
+  db.run(
+    `CREATE TABLE IF NOT EXISTS user_fiends(
+    user_id INTEGER NOT NULL,
+    friend_id INTEGER NOT NULL,
+    PRIMARY KEY (user_id, friend_id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (friend_id) REFERENCES users(id)
+  )`,
+    (err) => {
+      if (err) {
+        return console.error("Error creating table:", err.message);
+      }
+      console.log("Friend table ready.");
     },
   );
 });
