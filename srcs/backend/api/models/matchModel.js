9c5604b1ff6cee1e894598301e4b60ce7b95b41d
Álvaro Vega Gómez <alvarvg@gmail.com>
2025-02-06 13:00:39 +0100

    added match endpoint and model

--- srcs/backend/api/models/matchModel.js
+++ srcs/backend/api/models/matchModel.js
@@ -1,15 +1,130 @@
 import db from "../database.js";
 
-export function getMatches() {
+export function getMatchs() {
   return new Promise((resolve, reject) => {
     const sql = "SELECT * FROM matches";
 
     db.all(sql, (err, rows) => {
       if (err) {
-        console.error("Error getting match:", err.message);
+        console.error("Error getting matches:", err.message);
         return reject(err);
       }
       resolve(rows);
+    });
+  });
+}
+
+export function createMatch(data) {
+  return new Promise((resolve, reject) => {
+    const sql = `INSERT INTO matches (
+    left_player_id, right_player_id, result, winner_id, loser_id)
+    VALUES (?,?,?,?,?)`;
+    const params = [
+      data.left_player_id,
+      data.right_player_id,
+      data.result,
+      data.winner_id,
+      data.loser_id,
+    ];
+
+    db.run(sql, params, function (err) {
+      if (err) {
+        console.error("Error inserting match:", err.message);
+        return reject(err);
+      }
+      resolve({
+        id: this.lastID,
+        left_player_id: data.left_player_id,
+        right_player_id: data.right_player_id,
+        result: data.result,
+        winner_id: data.winner_id,
+        loser_id: data.loser_id,
+      });
+    });
+  });
+}
+
+export function getMatchByID(id) {
+  return new Promise((resolve, reject) => {
+    const sql = "SELECT * FROM matches WHERE id = ?";
+
+    db.get(sql, [id], (err, row) => {
+      if (err) {
+        console.error("Error getting match:", err.message);
+        return reject(err);
+      }
+      resolve(row);
+    });
+  });
+}
+
+export function putMatch(id, data) {
+  return new Promise((resolve, reject) => {
+    const sql = `
+      UPDATE matches
+      SET left_player_id = ?, right_player_id = ?, result = ?, winner_id = ?, loser_id = ?
+      WHERE id = ?
+    `;
+    const params = [
+      data.left_player_id,
+      data.right_player_id,
+      data.result,
+      data.winner_id,
+      data.loser_id,
+      id,
+    ];
+    db.run(sql, params, function (err) {
+      if (err) {
+        console.error("Error updating match:", err.message);
+        return reject(err);
+      }
+      if (this.changes === 0) {
+        return reject(new Error("Match not found"));
+      }
+      resolve(getMatchByID(id));
+    });
+  });
+}
+
+export function patchMatch(id, updates) {
+  return new Promise((resolve, reject) => {
+    const fields = Object.keys(updates)
+      .map((key) => `${key} = ?`)
+      .join(", ");
+    const params = Object.values(updates);
+    params.push(id);
+    const sql = `
+      UPDATE matches
+      SET ${fields}
+      WHERE id = ?
+    `;
+    db.run(sql, params, function (err) {
+      if (err) {
+        console.error("Error updating match:", err.message);
+        return reject(err);
+      }
+      if (this.changes === 0) {
+        return reject(new Error("Match not found"));
+      }
+      resolve({ id, ...updates });
+    });
+  });
+}
+export function deleteMatch(id) {
+  return new Promise((resolve, reject) => {
+    const sql = `
+      DELETE FROM matches
+      WHERE id = ?
+    `;
+    db.run(sql, id, function (err) {
+      if (err) {
+        console.error("Error deleting match:", err.message);
+        return reject(err);
+      }
+      if (this.changes === 0) {
+        return reject(new Error("Match not found"));
+      }
+      resolve();
     });
   });
 }
--- srcs/backend/api/routes/matchRoutes.js
+++ srcs/backend/api/routes/matchRoutes.js
@@ -1,11 +1,11 @@
 import { asyncHandler, validateInput } from "../utils.js";
 import {
-  getMatches,
-  //   createMatch,
-  //   getMatchByID,
-  //   putMatch,
-  //   patchMatch,
-  //   deleteMatch,
+  createMatch,
+  getMatchByID,
+  getMatchs,
+  putMatch,
+  patchMatch,
+  deleteMatch,
 } from "../models/matchModel.js";
 
 const match_routes = [
@@ -13,53 +13,71 @@ const match_routes = [
     method: "GET",
     url: "/matches",
     handler: asyncHandler(async (req, res) => {
-      const matches = await getMatches();
-      res.code(200).send(matches);
+      const matchs = await getMatchs();
+      res.code(200).send(matchs);
     }),
   },
-  // {
-  //   method: "POST",
-  //   url: "/matches",
-  //   handler: asyncHandler(async (req, res) => {
-  //     const match = await createMatch(req.body);
-  //     res.code(201).send(match);
-  //   }),
-  // },
-  // {
-  //   method: "GET",
-  //   url: "/matches/:id",
-  //   handler: asyncHandler(async (req, res) => {
-  //     const match = await getMatchByID(req.params.id);
-  //     if (!match) {
-  //       return res.code(404).send({ error: "Match with ID not found." });
-  //     }
-  //     res.code(200).send(match);
-  //   }),
-  // },
-  // {
-  //   method: "PUT",
-  //   url: "/matches/:id",
-  //   handler: asyncHandler(async (req, res) => {
-  //     const match = await putMatch(req.params.id, req.body);
-  //     res.code(200).send(match);
-  //   }),
-  // },
-  // {
-  //   method: "PATCH",
-  //   url: "/matches/:id",
-  //   handler: asyncHandler(async (req, res) => {
-  //     const match = await patchMatch(req.params.id, req.body);
-  //     res.code(200).send(match);
-  //   }),
-  // },
-  // {
-  //   method: "DELETE",
-  //   url: "/matches/:id",
-  //   handler: asyncHandler(async (req, res) => {
-  //     await deleteMatch(req.params.id);
-  //     res.code(204);
-  //   }),
-  // },
+  {
+    method: "POST",
+    url: "/matches",
+    handler: asyncHandler(async (req, res) => {
+      if (
+        !validateInput(req, res, [
+          "right_player_id",
+          "left_player_id",
+          "result",
+          "winner_id",
+          "loser_id",
+        ])
+      )
+        return;
+      const match = await createMatch(req.body);
+      res.code(201).send(match);
+    }),
+  },
+  {
+    method: "GET",
+    url: "/matches/:id",
+    handler: asyncHandler(async (req, res) => {
+      const match = await getMatchByID(req.params.id);
+      res.code(200).send(match);
+    }),
+  },
+  {
+    method: "PUT",
+    url: "/matches/:id",
+    handler: asyncHandler(async (req, res) => {
+      if (
+        !validateInput(req, res, [
+          "right_player_id",
+          "left_player_id",
+          "result",
+          "winner_id",
+          "loser_id",
+        ])
+      )
+        return;
+      const match = await putMatch(req.params.id, req.body);
+      res.code(200).send(match);
+    }),
+  },
+  {
+    method: "PATCH",
+    url: "/matches/:id",
+    handler: asyncHandler(async (req, res) => {
+      if (!validateInput(req, res, [])) return;
+      const match = await patchMatch(req.params.id, req.body);
+      res.code(200).send(match);
+    }),
+  },
+  {
+    method: "DELETE",
+    url: "/matches/:id",
+    handler: asyncHandler(async (req, res) => {
+      await deleteMatch(req.params.id);
+      res.code(204);
+    }),
+  },
 ];
 
 export default match_routes;
