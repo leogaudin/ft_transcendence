7e7ec4547ca9a59cced830f60e9c4bf65b9afa81
albagar4 <alba.gar.diaz@gmail.com>
2025-03-17 15:41:12 +0100

    refactor(frontend): more modular router

--- srcs/frontend/router.js
+++ srcs/frontend/router.js
@@ -4,20 +4,46 @@ import { initTwoFactorEvents } from "./components/two-factor-page/two-factor.js"
 import { displayToast } from "./components/toast-alert/toast-alert.js";
 
 const routes = [
-	{ path: "/", url: ""},
-	{ path: "/home",url: "./components/home-page/home-page.html"},
-	{ path: "/login",url: "./components/login-page/login-page.html"},
-	{ path: "/reset-password", url: "./components/reset-password-page/reset-password.html" },
-	{ path: "/two-factor", url: "./components/two-factor-page/two-factor.html"}
+	{ 
+		path: "/", 
+		url: "", 
+		event: () => {} 
+	},
+	{ 
+		path: "/home", 
+		url: "./components/home-page/home-page.html", 
+		event: () => {} 
+	},
+	{ 
+		path: "/login", 
+		url: "./components/login-page/login-page.html", 
+		event: () => {
+			initLoginEvents()
+		} 
+	},
+	{ 
+		path: "/reset-password", 
+		url: "./components/reset-password-page/reset-password.html", 
+		event: () => {
+			initResetPasswordEvents()
+		}
+	},
+	{ 
+		path: "/two-factor", 
+		url: "./components/two-factor-page/two-factor.html", 
+		event: (data) => {
+			initTwoFactorEvents(data)
+		} 
+	}
 ];
 
-export function navigateTo(path, argument = null) {
+export function navigateTo(path, data = null) {
 	console.log(`Navegando a: ${path}`);
 	history.pushState(null, "", path);
-	loadContent(path, argument);
+	loadContent(path, data);
 }
 
-async function loadContent(path, argument = null) {
+async function loadContent(path, data = null) {
 	try {
 		const route = routes.find(r => r.path === path);
 		if (!route)
@@ -26,31 +52,10 @@ async function loadContent(path, argument = null) {
 		const response = await fetch(route.url);
 		const content = await response.text();
 		document.getElementById("app").innerHTML = content;
-		loadEvents(path, argument);
+		route.event(data);
 	}
 	catch (error) {
 		console.error("Error al cargar la página:", error);
-	}
-}
-
-
-function loadEvents(path, argument = null) {
-	switch (path) {
-		case "/":
-			console.log("executing events for home");
-			break;
-		case "/login":
-			initLoginEvents();
-			break;
-		case "/reset-password":
-			initResetPasswordEvents();
-			break;
-		case "/two-factor":
-			initTwoFactorEvents(argument);
-			break;
-		// default:
-		//     loadNotFoundPage();
-		//     break;
 	}
 }
 
@@ -68,13 +73,11 @@ async function initBaseEvents() {
 			
 // Managing back and forward button
 window.onpopstate = () => {
-	console.log("I entered in onpopstate, window.location.pathname: ", window.location.pathname);
 	loadContent(window.location.pathname);
 };
 
 // Load page correctly when writing it directly on navbar
 document.addEventListener("DOMContentLoaded", () => {
-	console.log("window.location.pathname: ", window.location.pathname);
 	initBaseEvents();
 	loadContent(window.location.pathname);
 });
