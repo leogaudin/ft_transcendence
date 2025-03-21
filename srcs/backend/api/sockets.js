dde89b2480252244bc630e5f5e0d13a1e73edccb
ncruz-ga <ncruz-ga@student.42malaga.com>
2025-03-21 21:28:04 +0100

    Sockets funcionales pero no vinculados

--- srcs/backend/api/sockets.js
+++ srcs/backend/api/sockets.js
@@ -3,12 +3,12 @@ import { asyncHandler } from "./utils.js";
 export default function createWebSocketsRoutes(fastify){
 	return [
 		{
-			url: "/chat",
+			url: "/messages",
 			method: "GET",
 			websocket: true,
 			handler: asyncHandler(async (socket, req) =>{
 				socket.on("message", message => {
-					socket.send(message.toString());
+					console.log(message)
 				})
 			})
 		}
--- srcs/frontend/src/messages/messages-page.ts
+++ srcs/frontend/src/messages/messages-page.ts
@@ -4,6 +4,9 @@ import { Chat } from "../types.js"
 export function initMessagesEvents() {
 	moveToHome();
 	recentChats();
+  writeMessage();
+  initializeChat();
+  setupMessageForm();
 }
 
 function moveToHome() {
@@ -20,7 +23,7 @@ function recentChats() {
   let chats = localStorage.getItem("chats");
   if (!chats)
     return;
-  
+
   const JSONchats = JSON.parse(chats);
   const recentChatsDiv = document.getElementById("conversation-list");
   if (recentChatsDiv) {
@@ -47,4 +50,62 @@ function recentChats() {
       recentChatsDiv.appendChild(subDiv);
     });
   }
+}
+
+//Puede ser el fallo por la red de docker ¿host.docker.internal?
+function initializeChat() {
+  const socket = new WebSocket("ws://localhost:9000/messages");
+
+  socket.onopen = () => {
+    console.log("WebSocket connection established");
+    // Any initialization logic
+  };
+
+  socket.onmessage = (event) => {
+    console.log("Message received:", event.data);
+  };
+
+  socket.onerror = (error) => {
+    console.error("WebSocket error:", error);
+  };
+
+  console.log(socket);
+  socket.onclose = () => {
+    console.log("WebSocket connection closed");
+  };
+}
+
+function writeMessage() {
+  let chats = localStorage.getItem("chats");
+  if (!chats)
+      return;
+
+  const JSONchats = JSON.parse(chats);
+  Object.entries(JSONchats).forEach(([index, chat]) => {
+    const chatData = chat as Chat;
+    console.log(chatData);
+  })
+}
+
+function setupMessageForm() {
+  const messageForm = document.getElementById("message-box") as HTMLFormElement;
+  if (!messageForm) return;
+
+  messageForm.addEventListener("submit", function(event) {
+    // Evitar el comportamiento predeterminado (recargar la página)
+    event.preventDefault();
+    
+    // Obtener el input dentro del formulario
+    const input = messageForm.querySelector("input") as HTMLInputElement;
+    if (!input) return;
+    
+    const message = input.value.trim();
+    if (message) {
+      // Enviar el mensaje mediante WebSocket
+      const socket = initializeChat(); // Función para obtener tu socket activo
+      
+      // Limpiar el input después de enviar
+      input.value = "";
+    }
+  });
 }
