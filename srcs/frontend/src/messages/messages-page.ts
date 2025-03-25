b111a5765ce33b7731f8755ddf1cb708cd7cd7ea
Alba <albagar4@student.42malaga.com>
2025-03-25 11:13:42 +0100

    styles(frontend): better display for messages

--- srcs/frontend/src/messages/messages-page.ts
+++ srcs/frontend/src/messages/messages-page.ts
@@ -1,10 +1,11 @@
 import { Socket } from "socket.io";
+import { loadInfo } from "./load-info.js";
 import { navigateTo } from "../index.js";
 import { Chat } from "../types.js"
 
 export function initMessagesEvents() {
 	moveToHome();
-	recentChats();
+	loadInfo();
   initializeChat();
   setupMessageForm();
 }
@@ -17,39 +18,6 @@ function moveToHome() {
 	homeButton.addEventListener("click", () => {
 		navigateTo("/home");
 	});
-}
-
-function recentChats() {
-  let chats = localStorage.getItem("chats");
-  if (!chats)
-    return;
-
-  const JSONchats = JSON.parse(chats);
-  const recentChatsDiv = document.getElementById("conversation-list");
-  if (recentChatsDiv) {
-    Object.entries(JSONchats).forEach(([index, chat]) => {
-      const typedChat = chat as Chat;
-      var subDiv = document.createElement('div');
-      subDiv.innerHTML = `
-        <div class="flex items-center gap-2 recent-chat-card">
-			<div id="chat-avatar">
-				<img class="rounded-full" src="../../resources/img/cat.jpg" alt="Avatar">
-			</div>
-			<div class="chat-info">
-				<h3>${typedChat.receiver}</h3>
-			</div>
-		</div>
-      `;
-      
-    //   // Add click event to open this chat
-    //   subDiv.addEventListener('click', () => {
-    //     // Navigate to chat or load conversation
-    //     console.log(`Opening chat with ${typedChat.first_username}, chat ID: ${typedChat.chat_id}`);
-    //   });
-      
-      recentChatsDiv.appendChild(subDiv);
-    });
-  }
 }
 
 function initializeChat() {
