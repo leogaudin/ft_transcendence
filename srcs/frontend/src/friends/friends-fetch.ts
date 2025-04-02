31ca53291780c4537473d4d269a6a8b403d624e2
Alba <albagar4@student.42malaga.com>
2025-04-02 16:41:59 +0200

    feat(frontend): displaying user matches

--- srcs/frontend/src/friends/friends-fetch.ts
+++ srcs/frontend/src/friends/friends-fetch.ts
@@ -1,5 +1,5 @@
 import { UserMatches } from "../types.js"
-import { sendRequest } from "../login-page/login-fetch";
+import { sendRequest } from "../login-page/login-fetch.js";
 
 export function initFriendFetches() {
 	const friendInput = document.getElementById("friend-input") as HTMLInputElement;
@@ -18,21 +18,41 @@ export function initFriendFetches() {
 			}
 		  };
 		  friendInput.oninput = debounce(() => {showMatches(friendInput.value)}, 500);
-			// for (let option of searchFriend.options) {
-			//   if(option.value.toUpperCase().indexOf(text) > -1){
-			// 	option.style.display = "block";
-			// }else{
-			//   option.style.display = "none";
-			//   }
-			// };
-		
+	}
+}
+
+function emptyMatches(datalist: HTMLElement) {
+	var prevOptions = datalist.querySelectorAll(".match-option");
+	if (prevOptions) {
+		prevOptions.forEach((option) => {
+			option.remove();
+		})
 	}
 }
 
 async function showMatches(input: string) {
-	console.log(input);
+	const datalist = document.getElementById("search-friend");
 
-	const matches = await sendRequest('POST', '/users/search');
+	if (datalist) {
+		emptyMatches(datalist);
+		if (!input || input.length === 0)
+			return ;
+	
+		const matchesTyped = await sendRequest('POST', '/users/search', {username: input}) as UserMatches[];
+		for(let i = 0, j = 0; (i < matchesTyped.length && i < 5) || j === 0; i++, j++) {
+			var option;
+			if (matchesTyped.length === 0) {
+				option = document.createElement('p');
+				option.innerText = "User Not Found";
+			}
+			else {
+				option = document.createElement('option');
+				option.innerHTML = `${matchesTyped[i].username}`;
+			}
+			option.setAttribute("class", "match-option");
+			datalist.appendChild(option);
+		}
+	}
 }
 
 function debounce(callback: Function, wait: number) {
