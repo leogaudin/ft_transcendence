05a14cada271a1cf576372810d7e6d177ba12902
Álvaro Vega Gómez <alvarvg@gmail.com>
2025-05-05 16:21:16 +0200

    docs(front): comment on avatar

--- srcs/frontend/src/friends/friends-fetch.ts
+++ srcs/frontend/src/friends/friends-fetch.ts
@@ -279,7 +279,8 @@ export async function displayFriends() {
 			section.setAttribute("class", "friend-card");
 			section.innerHTML = `
 						<div class="flex items-center gap-4">
-							<img id="friend-avatar" class="card-avatar rounded-full m-1.5" src="../../resources/img/cat.jpg" alt="Avatar">
+              <!-- Se puede poner el avatar de un usuario de la siguiente forma:  -->
+							<img id="friend-avatar" class="card-avatar rounded-full m-1.5" src="${friend.avatar}" alt="Avatar">
 							<div class="flex flex-col">
 								<h3>${friend.username}</h3>
 								<p class="opacity-50 text-sm">${friend.status}</p>
